32d2404e6eae2e3c3ed74b7792cd3e93f5aae884
Joshua Membreno <membrenojoshua@gmail.com>
2018-09-19 20:32:28 -0500

    Source code

---
+++ todo-app/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Todo } from "./todo";
+import { TodoDataService } from './todo-data.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: []
+})
+
+export class AppComponent {
+
+  constructor(private todoDataService: TodoDataService){
+  
+  }
+  
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
+  }
+
+  onToggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  onRemoveTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
+}
